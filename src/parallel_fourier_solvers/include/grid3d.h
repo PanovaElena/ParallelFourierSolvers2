4bd6c92843c9c5d8aa552d1b10370eb5adc62c78
Alyona <alyona-gra98@yandex.ru>
2019-09-26 23:04:49 +0300

    checked grid

--- src/parallel_fourier_solvers/include/grid3d.h
+++ src/parallel_fourier_solvers/include/grid3d.h
@@ -7,19 +7,6 @@
 template <class T>
 struct FieldForGrid : public vec3<Array3d<T>> {
 
-    vec3<T> operator()(int i, int j, int k)
-    {
-        return vec3<T>(this->x(i, j, k), this->y(i, j, k), this->z(i, j, k));
-    }
-    vec3<T> operator()(vec3<int> v)
-    {
-        return (*this)(v.x, v.y, v.z);
-    }
-    void write(int i, int j, int k, vec3<T> val) {
-        this->x(i, j, k) = val.x;
-        this->y(i, j, k) = val.y;
-        this->z(i, j, k) = val.z;
-    }
     void clear() {
         this->x.clear();
         this->y.clear();
