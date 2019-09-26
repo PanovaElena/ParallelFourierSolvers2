d6962fd3eaa325a09395adec0dac8b4b601f9ef2
Alyona <alyona-gra98@yandex.ru>
2019-09-26 12:40:56 +0300

    tmp commit

---
+++ cmake/FindMKL.cmake
@@ -0,0 +1,143 @@
+#  Find the Math Kernel Library from Intel
+#
+#  MKL_FOUND - System has MKL
+#  MKL_INCLUDE_DIRS - MKL include files directories
+#  MKL_LIBRARIES - MKL libraries
+#
+#  The environment variable MKLROOT is used
+#
+#  Example usage:
+#
+#  find_package(MKL)
+#  if(MKL_FOUND)
+#    target_link_libraries(TARGET ${MKL_LIBRARIES})
+#  endif()
+
+# If already in cache, be silent
+if (MKL_INCLUDE_DIRS AND MKL_LIBRARIES)
+    set (MKL_FIND_QUIETLY TRUE)
+endif()
+
+set(MKLROOT $ENV{MKLROOT})
+    
+find_path(MKL_INCLUDE_DIR NAMES mkl.h HINTS ${MKLROOT}/include)
+set(MKL_INCLUDE_DIRS ${MKL_INCLUDE_DIR} ${MKL_INCLUDE_DIR}/fftw)    
+set(MKL_LIBRARIES_PATHS ${MKLROOT}/lib ${MKLROOT}/lib/intel64)
+
+if (USE_OMP)
+
+    function(find_libomp5_win32)
+        find_library(IOMP5_LIB libiomp5md${CMAKE_STATIC_LIBRARY_SUFFIX} $ENV{INTEL}/compiler/lib/intel64)
+        if (IOMP5_LIB)
+            set(IOMP5 ${IOMP5_LIB} PARENT_SCOPE)
+            message(STATUS "Found libiomp5")
+        else()
+            message(WARNING "Cannot find libiomp5")
+        endif()
+    endfunction()
+
+    # static linking is preferred
+    find_library( MKL_CDFT_CORE_LIB NAMES ${CMAKE_STATIC_LIBRARY_PREFIX}mkl_cdft_core${CMAKE_STATIC_LIBRARY_SUFFIX} mkl_cdft_core PATHS ${MKL_LIBRARIES_PATHS} )
+    find_library( MKL_INTEL_ILP64_LIB NAMES ${CMAKE_STATIC_LIBRARY_PREFIX}mkl_intel_ilp64${CMAKE_STATIC_LIBRARY_SUFFIX} mkl_intel_ilp64 PATHS ${MKL_LIBRARIES_PATHS} )
+    find_library( MKL_INTEL_THREAD_LIB NAMES ${CMAKE_STATIC_LIBRARY_PREFIX}mkl_intel_thread${CMAKE_STATIC_LIBRARY_SUFFIX} mkl_intel_thread PATHS ${MKL_LIBRARIES_PATHS} )
+    find_library( MKL_CORE_LIB NAMES ${CMAKE_STATIC_LIBRARY_PREFIX}mkl_core${CMAKE_STATIC_LIBRARY_SUFFIX} mkl_core PATHS ${MKL_LIBRARIES_PATHS} )
+    find_library( MKL_BLACS_INTELMPI_ILP64_LIB NAMES ${CMAKE_STATIC_LIBRARY_PREFIX}mkl_blacs_intelmpi_ilp64${CMAKE_STATIC_LIBRARY_SUFFIX} mkl_blacs_intelmpi_ilp64 PATHS ${MKL_LIBRARIES_PATHS} )
+    
+    if (MKL_INCLUDE_DIRS AND
+         MKL_CDFT_CORE_LIB AND
+         MKL_INTEL_ILP64_LIB AND
+         MKL_INTEL_THREAD_LIB AND 
+         MKL_CORE_LIB AND
+         MKL_BLACS_INTELMPI_ILP64_LIB)
+         
+        set(MKL_LIBRARIES "${MKL_CDFT_CORE_LIB} ${MKL_INTEL_ILP64_LIB} ${MKL_INTEL_THREAD_LIB} \
+            ${MKL_CORE_LIB} ${MKL_BLACS_INTELMPI_ILP64_LIB}")
+        
+        # ICC
+        if (${CMAKE_CXX_COMPILER} MATCHES "icc.*$" OR ${CMAKE_CXX_COMPILER} MATCHES "icl.*$")
+            if (WIN32)
+                find_libomp5_win32()
+                set(MKL_LIBRARIES ${MKL_LIBRARIES} ${IOMP5})
+                set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \DMKL_ILP64")
+            elseif(UNIX)
+                set(MKL_LIBRARIES "-Wl,--start-group ${MKL_LIBRARIES} -Wl,--end-group -liomp5 -lpthread -lm -ldl")
+                set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DMKL_ILP64")
+            endif()
+        endif()
+        
+        # GCC
+        if (${CMAKE_CXX_COMPILER} MATCHES "gcc.*$")
+            set(MKL_LIBRARIES "-Wl,--start-group ${MKL_LIBRARIES} -Wl,--end-group -lgomp -lpthread -lm -ldl")
+            set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DMKL_ILP64")
+        endif()
+        
+        # MSVC
+        if (MSVC AND NOT ${CMAKE_CXX_COMPILER} MATCHES "icl.*$") 
+            find_libomp5_win32()
+            set(MKL_LIBRARIES ${MKL_LIBRARIES} ${IOMP5})
+            set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \DMKL_ILP64")
+        endif()
+           
+        # Handle the QUIETLY and REQUIRED arguments and set MKL_FOUND to TRUE if
+        # all listed variables are TRUE.
+        INCLUDE(FindPackageHandleStandardArgs)
+        FIND_PACKAGE_HANDLE_STANDARD_ARGS(MKL DEFAULT_MSG MKL_LIBRARIES MKL_INCLUDE_DIRS)
+        
+        MARK_AS_ADVANCED(MKL_INCLUDE_DIRS MKL_LIBRARIES)
+        
+    else()
+        message(WARNING "Cannot find some necessary mkl components")
+        message(STATUS "MKL_INCLUDE_DIRS=${MKL_INCLUDE_DIRS}")
+        message(STATUS "MKL_CDFT_CORE_LIB=${MKL_CDFT_CORE_LIB}")
+        message(STATUS "MKL_INTEL_ILP64_LIB=${MKL_INTEL_ILP64_LIB}")
+        message(STATUS "MKL_INTEL_THREAD_LIB=${MKL_INTEL_SEQUENTIAL_LIB}")
+        message(STATUS "MKL_CORE_LIB=${MKL_CORE_LIB}")
+        message(STATUS "MKL_BLACS_INTELMPI_ILP64_LIB=${MKL_BLACS_INTELMPI_ILP64_LIB}")
+    endif()
+    
+    
+else()  # USE_OMP
+
+    # static linking is preferred
+    find_library( MKL_CDFT_CORE_LIB NAMES ${CMAKE_STATIC_LIBRARY_PREFIX}mkl_cdft_core${CMAKE_STATIC_LIBRARY_SUFFIX} mkl_cdft_core PATHS ${MKL_LIBRARIES_PATHS} )
+    find_library( MKL_INTEL_ILP64_LIB NAMES ${CMAKE_STATIC_LIBRARY_PREFIX}mkl_intel_ilp64${CMAKE_STATIC_LIBRARY_SUFFIX} mkl_intel_ilp64 PATHS ${MKL_LIBRARIES_PATHS} )
+    find_library( MKL_INTEL_SEQUENTIAL_LIB NAMES ${CMAKE_STATIC_LIBRARY_PREFIX}mkl_sequential${CMAKE_STATIC_LIBRARY_SUFFIX} mkl_sequential PATHS ${MKL_LIBRARIES_PATHS} )
+    find_library( MKL_CORE_LIB NAMES ${CMAKE_STATIC_LIBRARY_PREFIX}mkl_core${CMAKE_STATIC_LIBRARY_SUFFIX} mkl_core PATHS ${MKL_LIBRARIES_PATHS} )
+    find_library( MKL_BLACS_INTELMPI_ILP64_LIB NAMES ${CMAKE_STATIC_LIBRARY_PREFIX}mkl_blacs_intelmpi_ilp64${CMAKE_STATIC_LIBRARY_SUFFIX} mkl_blacs_intelmpi_ilp64 PATHS ${MKL_LIBRARIES_PATHS} )
+        
+    if (MKL_INCLUDE_DIRS AND
+         MKL_CDFT_CORE_LIB AND
+         MKL_INTEL_ILP64_LIB AND
+         MKL_INTEL_SEQUENTIAL_LIB AND 
+         MKL_CORE_LIB AND
+         MKL_BLACS_INTELMPI_ILP64_LIB)
+         
+        set(MKL_LIBRARIES "${MKL_CDFT_CORE_LIB} ${MKL_INTEL_ILP64_LIB} ${MKL_INTEL_SEQUENTIAL_LIB} \
+            ${MKL_CORE_LIB} ${MKL_BLACS_INTELMPI_ILP64_LIB}")
+                
+        if (UNIX)
+            set(MKL_LIBRARIES "-Wl,--start-group ${MKL_LIBRARIES} -Wl,--end-group -lpthread -lm -ldl")
+            set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DMKL_ILP64")
+        elseif(WIN32)
+            set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /DMKL_ILP64")
+        endif()
+           
+        # Handle the QUIETLY and REQUIRED arguments and set MKL_FOUND to TRUE if
+        # all listed variables are TRUE.
+        INCLUDE(FindPackageHandleStandardArgs)
+        FIND_PACKAGE_HANDLE_STANDARD_ARGS(MKL DEFAULT_MSG MKL_LIBRARIES MKL_INCLUDE_DIRS)
+        
+        MARK_AS_ADVANCED(MKL_INCLUDE_DIRS MKL_LIBRARIES)
+        
+    else()
+        message(WARNING "Cannot find some necessary mkl components")
+        message(STATUS "MKL_INCLUDE_DIRS=${MKL_INCLUDE_DIRS}")
+        message(STATUS "MKL_CDFT_CORE_LIB=${MKL_CDFT_CORE_LIB}")
+        message(STATUS "MKL_INTEL_ILP64_LIB=${MKL_INTEL_ILP64_LIB}")
+        message(STATUS "MKL_INTEL_SEQUENTIAL_LIB=${MKL_INTEL_SEQUENTIAL_LIB}")
+        message(STATUS "MKL_CORE_LIB=${MKL_CORE_LIB}")
+        message(STATUS "MKL_BLACS_INTELMPI_ILP64_LIB=${MKL_BLACS_INTELMPI_ILP64_LIB}")
+    endif()
+    
+
+endif()  # USE_OMP
---
+++ cmake/function.cmake
@@ -0,0 +1,110 @@
+function( add_element_to_global_property NAME_PROPERTY ELEMENT )
+    get_property( PROP GLOBAL PROPERTY ${NAME_PROPERTY} )
+    set(PROP ${PROP} ${ELEMENT} )
+    set_property ( GLOBAL PROPERTY ${NAME_PROPERTY} ${PROP} )
+    
+endfunction()
+
+
+# creating libs
+function( create_library TARGET )
+    file( GLOB TARGET_SRC "src/*.cpp" "src/*.cc" "include/*.h" )
+    add_library( ${TARGET} STATIC ${TARGET_SRC} )
+    
+    target_include_directories( ${TARGET} PUBLIC
+        ${CMAKE_CURRENT_SOURCE_DIR}/include
+        ${FFT_INCLUDES}
+        ${MPI_CXX_INCLUDE_DIRS})
+    
+    add_element_to_global_property( MY_INCLUDES ${CMAKE_CURRENT_SOURCE_DIR}/include )
+    add_element_to_global_property( MY_TEST_INCLUDES ${CMAKE_CURRENT_SOURCE_DIR}/include )
+    add_element_to_global_property( MY_LIBRARIES ${TARGET} )
+    
+endfunction()
+
+
+# creating executable
+function( create_executable TARGET)
+    if ( EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/src" )
+        set( TARGET_SRC_FILES ${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_SOURCE_DIR}/src/*.cpp )
+    endif()
+    if ( EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/include" )
+        set( TARGET_INCLUDE_DIRS ${TARGET_INCLUDE_DIRS} ${CMAKE_CURRENT_SOURCE_DIR}/include )
+        set( TARGET_INCLUDE_FILES ${TARGET_INCLUDE_FILES} ${CMAKE_CURRENT_SOURCE_DIR}/include/*.h )
+    endif()
+    if ( EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/../include" )
+        set( TARGET_INCLUDE_DIRS ${TARGET_INCLUDE_DIRS} ${CMAKE_CURRENT_SOURCE_DIR}/../include )
+        set( TARGET_INCLUDE_FILES ${TARGET_INCLUDE_FILES} ${CMAKE_CURRENT_SOURCE_DIR}/../include/*.h )
+    endif()
+    if ( EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/../../include" )
+        set( TARGET_INCLUDE_DIRS ${TARGET_INCLUDE_DIRS} ${CMAKE_CURRENT_SOURCE_DIR}/../../include )
+        set( TARGET_INCLUDE_FILES ${TARGET_INCLUDE_FILES} ${CMAKE_CURRENT_SOURCE_DIR}/../../include/*.h )
+    endif()
+    file( GLOB TARGET_SRC ${TARGET_SRC_FILES} ${TARGET_INCLUDE_FILES} )
+        
+    add_executable( ${TARGET} ${TARGET_SRC} )
+    
+    if (USE_FFTW AND NOT FFTW_DIR) 
+        add_dependencies( ${TARGET} project_fftw )
+    endif()
+     
+    get_property( MY_LIB_INCLUDES GLOBAL PROPERTY MY_INCLUDES )
+    target_include_directories( ${TARGET} PUBLIC
+        ${TARGET_INCLUDE_DIRS}
+        ${MY_LIB_INCLUDES}
+        ${FFT_INCLUDES}
+        ${MPI_CXX_INCLUDE_DIRS})
+    
+    get_property( MY_LIBS GLOBAL PROPERTY MY_LIBRARIES )
+    if ( UNIX )
+        target_link_libraries( ${TARGET} #-static
+            ${MY_LIBS}
+            ${FFT_LIBRARIES}
+            ${MPI_CXX_LIBRARIES})
+    elseif( WIN32 )
+        target_link_libraries( ${TARGET}
+            ${MY_LIBS}
+            ${FFT_LIBRARIES}
+            ${MPI_CXX_LIBRARIES})
+    endif()
+    
+    add_element_to_global_property( MY_TEST_INCLUDES "${TARGET_INCLUDE_DIRS}" )
+
+endfunction()
+
+
+# creating test project
+function( create_tests )
+    set( TARGET tests )
+    if ( EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/src" )
+        set( TARGET_SRC_FILES ${CMAKE_CURRENT_SOURCE_DIR}/src/*.cpp )
+    endif()
+    file( GLOB TARGET_SRC ${TARGET_SRC_FILES} )
+        
+    add_executable( ${TARGET} ${TARGET_SRC} )
+    
+    if (USE_FFTW AND NOT FFTW_DIR) 
+        add_dependencies( ${TARGET} project_fftw )
+    endif()
+    
+    get_property( MY_ALL_TEST_INCLUDES GLOBAL PROPERTY MY_TEST_INCLUDES )
+    target_include_directories( ${TARGET} PUBLIC
+        ${MY_ALL_TEST_INCLUDES}
+        ${FFT_INCLUDES}
+        ${MPI_CXX_INCLUDE_DIRS})
+    
+    get_property( MY_LIBS GLOBAL PROPERTY MY_LIBRARIES )
+    if ( UNIX )
+        target_link_libraries( ${TARGET} #-static
+            ${MY_LIBS}
+            ${FFT_LIBRARIES}
+            ${MPI_CXX_LIBRARIES})
+    elseif( WIN32 )
+        target_link_libraries( ${TARGET}
+            ${MY_LIBS}
+            ${FFT_LIBRARIES}
+            ${MPI_CXX_LIBRARIES})
+    endif()
+
+endfunction()
+
