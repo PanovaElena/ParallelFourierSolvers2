f8c8dec2e8f6b4526ffc459d4ac52f1d9baa18a2
Alyona <alyona-gra98@yandex.ru>
2019-10-07 14:04:38 +0300

    final version of array3d

--- src/parallel_fourier_solvers/include/array3d.h
+++ src/parallel_fourier_solvers/include/array3d.h
@@ -5,11 +5,7 @@
 template <class T>
 class Array3d {
     vec3<int> n;
-
-    std::vector<T> tmp1;  // structure for allocation of memory
-    std::vector<T*> tmp2;
-    std::vector<T**> data;
-
+    std::vector<T> data;
     void allocMem();
 
 public:
@@ -24,7 +20,7 @@ public:
         for (int i = 0; i < arr1.n.x; i++)
             for (int j = 0; j < arr1.n.y; j++)
                 for (int k = 0; k < arr1.n.z; k++)
-                    if (arr1.data[i][j][k] != arr2.data[i][j][k]) return 0;
+                    if (arr1.data[arr1.getIndex(i, j, k)] != arr2.data[arr2.getIndex(i, j, k)]) return 0;
         return 1;
     };
     friend int operator!=(const Array3d<T>& arr1, const Array3d<T>& arr2) {
@@ -42,6 +38,8 @@ public:
     T operator[](int index) const;
     T operator()(int index) const;
 
+    int getIndex(int i, int j, int k) const;
+
     Array3d& operator=(const Array3d& arr);
 
     T* getArray1d();
@@ -56,20 +54,13 @@ public:
 template<class T>
 inline void Array3d<T>::allocMem()
 {
-    tmp1.resize(n.x*n.y*n.z);
-    tmp2.resize(n.x*n.y);
-    data.resize(n.x);
-
-    for (int i = 0; i < n.x*n.y; i++)
-        tmp2[i] = &(tmp1[i*n.z]);
-    for (int i = 0; i < n.x; i++)
-        data[i] = &(tmp2[i*n.y]);
+    data.resize(n.x*n.y*n.z);
 }
 
 template<class T>
 inline void Array3d<T>::initialize(vec3<int> _n) {
     clear();
-    n.x = _n.x; n.y = _n.y; n.z = _n.z;
+    n = _n;
     allocMem();
     setToZeros();
 }
@@ -87,7 +78,7 @@ inline Array3d<T>::Array3d(const Array3d & arr) {
     for (int i = 0; i < n.x; i++)
         for (int j = 0; j < n.y; j++)
             for (int k = 0; k < n.z; k++)
-                (*this)(i, j, k) = arr.data[i][j][k];
+                (*this)(i, j, k) = arr.data[getIndex(i, j, k)];
 }
 
 template<class T>
@@ -97,52 +88,58 @@ inline Array3d<T>::Array3d(vec3<int> _n) {
 
 template<class T>
 inline T & Array3d<T>::operator()(int i, int j, int k) {
-    return data[i][j][k];
+    return data[getIndex(i, j, k)];
 }
 
 template<class T>
 inline T & Array3d<T>::operator()(vec3<int> index) {
-    return data[index.x][index.y][index.z];
+    return data[getIndex(index.x, index.y, index.z)];
 }
 
 template<class T>
 inline T & Array3d<T>::get(int i, int j, int k) {
-    return data[i][j][k];
+    return data[getIndex(i, j, k)];
 }
 
 template<class T>
 inline T & Array3d<T>::operator[](int index) {
-    return tmp1[index];
+    return data[index];
 }
 
 template<class T>
 inline T & Array3d<T>::operator()(int index) {
-    return tmp1[index];
+    return data[index];
 }
 
 template<class T>
 inline T Array3d<T>::operator()(int i, int j, int k) const {
-    return data[i][j][k];
+    return data[getIndex(i, j, k)];
 }
 
 template<class T>
 inline T Array3d<T>::operator()(vec3<int> index) const {
-    return data[index.x][index.y][index.z];
+    return data[getIndex(index.x, index.y, index.z)];
 }
 
 template<class T>
 inline T Array3d<T>::get(int i, int j, int k) const {
-    return data[i][j][k];
+    return data[getIndex(i, j, k)];
 }
 
 template<class T>
 inline T Array3d<T>::operator[](int index) const {
-    return tmp1[index];
+    return data[index];
 }
 
 template<class T>
 inline T Array3d<T>::operator()(int index) const {
-    return tmp1[index];
+    return data[index];
+}
+
+template<class T>
+inline int Array3d<T>::getIndex(int i, int j, int k) const
+{
+    return (i*n.x + j)*n.y + k;
 }
 
 template<class T>
@@ -152,25 +149,23 @@ inline Array3d<T> & Array3d<T>::operator=(const Array3d & arr) {
     for (int i = 0; i < n.x; i++)
         for (int j = 0; j < n.y; j++)
             for (int k = 0; k < n.z; k++)
-                (*this)(i, j, k) = arr.data[i][j][k];
+                (*this)(i, j, k) = arr.data[getIndex(i, j, k)];
 
     return *this;
 }
 
 template<class T>
 inline T * Array3d<T>::getArray1d() {
-    return tmp1;
+    return data;
 }
 
 template<class T>
 inline void Array3d<T>::clear() {
     data.resize(0);
-    tmp2.resize(0);
-    tmp1.resize(0);
 }
 
 template<class T>
 inline void Array3d<T>::setToZeros() {
     for (int i = 0; i < n.x*n.y*n.z; i++)
-        tmp1[i] = 0;
+        data[i] = 0;
 }
