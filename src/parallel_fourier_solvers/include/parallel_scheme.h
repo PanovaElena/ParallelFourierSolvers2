c211392bb924d0b6ac96aa13d960f0b408620c3d
Alyona <alyona-gra98@yandex.ru>
2019-10-07 14:12:57 +0300

    added parallel schemes

---
+++ src/parallel_fourier_solvers/include/parallel_scheme.h
@@ -0,0 +1,91 @@
+#pragma once
+#include "vector3d.h"
+#include "status.h"
+#include "simple_types.h"
+#include "mask.h"
+
+class ParallelScheme {
+protected:
+    vec3<int> domainSize;
+    vec3<int> guardSize;
+    vec3<Pair<Boards>> sendBoards;
+    vec3<Pair<Boards>> recvBoards;
+    Operation operation;
+
+public:
+    virtual ParallelScheme* clone() const = 0;
+    virtual Stat initialize(vec3<int> domainSize, vec3<int> guardSize) {
+        this->guardSize = guardSize;
+        this->domainSize = domainSize;
+        setBoardsForExchange();
+        return Stat::OK;
+    }
+
+    virtual void setBoardsForExchange() = 0;
+    virtual void setMask(const std::shared_ptr<Mask>& mask) = 0;
+
+    vec3<int> getGuardSize() {
+        return guardSize;
+    }
+    Pair<Boards> getSendBoards(Coordinate coord) {
+        return sendBoards[coord];
+    }
+    Pair<Boards> getRecvBoards(Coordinate coord) {
+        return recvBoards[coord];
+    }
+    vec3<Pair<Boards>> getSendBoards() {
+        return sendBoards;
+    }
+    vec3<Pair<Boards>> getRecvBoards() {
+        return recvBoards;
+    }
+    vec3<int> getDomainSize() {
+        return domainSize;
+    }
+    Operation getOperation() {
+        return operation;
+    }
+
+};
+
+
+class ParallelSchemeSum: public ParallelScheme {
+    Array3d<double> tmp;
+
+public:
+    ParallelSchemeSum(vec3<int> domainSize, vec3<int> guardSize) {
+        initialize(domainSize, guardSize);
+    }
+
+    Stat initialize(vec3<int> domainSize, vec3<int> guardSize) override;
+
+    void setBoardsForExchange() override;
+
+    void setMask(const std::shared_ptr<Mask>& mask) override {
+        mask->setDomainAndGuardSize(domainSize, guardSize);
+    }
+
+    virtual ParallelScheme* clone() const override {
+        return new ParallelSchemeSum(*this);
+    }
+};
+
+
+class ParallelSchemeCopy: public ParallelScheme {
+public:
+    ParallelSchemeCopy(vec3<int> domainSize, vec3<int> guardSize) {
+        initialize(domainSize, guardSize);
+    }
+
+    Stat initialize(vec3<int> domainSize, vec3<int> guardSize) override;
+
+    void setBoardsForExchange() override;
+
+    void setMask(const std::shared_ptr<Mask>& mask) override {
+        mask->setDomainAndGuardSize(domainSize + guardSize - guardSize / 2, guardSize / 2);
+    }
+
+    virtual ParallelScheme* clone() const override {
+        return new ParallelSchemeCopy(*this);
+    }
+};
---
+++ src/parallel_fourier_solvers/src/parallel_scheme.cpp
@@ -0,0 +1,57 @@
+#include "parallel_scheme.h"
+
+Stat ParallelSchemeSum::initialize(vec3<int> domainSize, vec3<int> guardSize)
+{
+    if (ParallelScheme::initialize(domainSize, guardSize) == Stat::ERROR)
+        return Stat::ERROR;
+    operation = Operation::sum;
+    return Stat::OK;
+}
+
+void ParallelSchemeSum::setBoardsForExchange()
+{
+    for (int i = 0; i < 3; i++) {
+        vec3<int> fullSize = domainSize + 2 * guardSize;
+        vec3<int> base = vec3<int>::getBaseVector(i);
+        vec3<int> invBase(base.x == 0 ? 1 : 0,
+            base.y == 0 ? 1 : 0, base.z == 0 ? 1 : 0);
+        vec3<int> fullInvBase(invBase*fullSize);
+
+        sendBoards[i].left.left = vec3<int>(0);
+        sendBoards[i].left.right = 2 * guardSize * base + fullInvBase;
+        sendBoards[i].right.left = domainSize * base;
+        sendBoards[i].right.right = fullSize * base + fullInvBase;
+        recvBoards[i].right.left = domainSize * base;
+        recvBoards[i].right.right = fullSize * base + fullInvBase;
+        recvBoards[i].left.left = vec3<int>(0);
+        recvBoards[i].left.right = 2 * guardSize * base + fullInvBase;
+    }
+}
+
+Stat ParallelSchemeCopy::initialize(vec3<int> domainSize, vec3<int> guardSize)
+{
+    if (ParallelScheme::initialize(domainSize, guardSize) == Stat::ERROR)
+        return Stat::ERROR;
+    operation = Operation::copy;
+    return Stat::OK;
+}
+
+void ParallelSchemeCopy::setBoardsForExchange()
+{
+    for (int i = 0; i < 3; i++) {
+        vec3<int> fullSize = domainSize + 2 * guardSize;
+        vec3<int> base = vec3<int>::getBaseVector(i);
+        vec3<int> invBase(base.x == 0 ? 1 : 0,
+            base.y == 0 ? 1 : 0, base.z == 0 ? 1 : 0);
+        vec3<int> fullInvBase(invBase*fullSize);
+
+        sendBoards[i].left.left = guardSize * base;
+        sendBoards[i].left.right = 2 * guardSize * base + fullInvBase;
+        sendBoards[i].right.left = domainSize * base;
+        sendBoards[i].right.right = (domainSize + guardSize) * base + fullInvBase;
+        recvBoards[i].left.left = vec3<int>(0);
+        recvBoards[i].left.right = guardSize * base + fullInvBase;
+        recvBoards[i].right.left = (domainSize + guardSize) * base;
+        recvBoards[i].right.right = fullSize;
+    }
+}
